588ed9aabf3aebef136a4bffb2cfa48545c65b7b
olii <oliver.nemcek@gmail.com>
2021-03-04 11:34:12 +0100

    Fix parsing of lists with relativedelta objects.
    The relativedelta objects were rejected because we cannot compare them to
    strings. Both `==` and `!=` results to `False`.
    
    Instead we have to first test the type and then do the comparison.

--- pyhocon/config_parser.py
+++ pyhocon/config_parser.py
@@ -751,8 +751,16 @@ class ListParser(TokenConverter):
         :return:
         """
         cleaned_token_list = []
+        # Note that a token can be a duration value object:
+        # >>> relativedelta(hours = 1) == ''
+        # False
+        # >>> relativedelta(hours = 1) != ''
+        # False
+        # relativedelta.__eq__() raises NotImplemented if it is compared with
+        # a different object type so Python falls back to identity comparison.
+        # We cannot compare this object to a string object.
         for token in token_list:
-            if token == '':
+            if isinstance(token, str) and token == '':
                 # This is the case when there was a trailing comma in the list.
                 # The last token is just an empty string so we can safely ignore
                 # it.
